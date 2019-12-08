d1ef02c156b661f17727bfea30adaa4c55259c4d
Kfir Levy <kfir258levy@gmail.com>
2019-12-08 22:07:16 +0200

    update for the pit scouting pages. can add numbers data

--- lib/TeamPitScouting.dart
+++ lib/TeamPitScouting.dart
@@ -15,36 +15,49 @@ class TeamPitScouting extends StatelessWidget {
   }
 }
 
-class PitScoutingPage extends StatelessWidget{
+class PitScoutingPage extends StatefulWidget{
+  final String title;
+  PitScoutingPage({Key key, this.title}) : super(key: key);
+
+  @override
+  TeamPage createState() => TeamPage();
+
+}
+
+class TeamPage extends State<PitScoutingPage> {
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        row("Robot Weight", "Pounds"),
-        row("Robot Width", "Inches"),
-        row("Robot Length", "Inches"),
-        row("DT Motors", "Amount"),
-
+        textFieldCreator("Robot Weight", "Pounds"),
+        textFieldCreator("Robot Width", "Inches"),
+        textFieldCreator("Robot Length", "Inches"),
+        textFieldCreator("DT Motors", "Amount"),
       ],
     );
   }
 
-  Widget row(String text, String kind){
+  Widget toggleButtonsCreator(){
+    // TO DO :
+    // make some kind of Toggle Buttons function that get an array
+  }
+
+  Widget textFieldCreator(String text, String kind){
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Flexible(
-        child: TextField(
-          style: TextStyle(fontSize: 15),
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.only(left: 50, right: 50, top: 20),
-            labelText: text,
-            labelStyle: TextStyle(color: Colors.blue, fontSize: 20),
-            hintText: kind,
-            border: InputBorder.none,
-            hintStyle: TextStyle(fontSize: 20),
+          child: TextField(
+            style: TextStyle(fontSize: 15),
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.only(left: 50, right: 50, top: 20),
+              labelText: text,
+              labelStyle: TextStyle(color: Colors.blue, fontSize: 20),
+              hintText: kind,
+              border: InputBorder.none,
+              hintStyle: TextStyle(fontSize: 20),
             ),
           ),
         ),
