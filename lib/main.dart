1066676a221307eeda070375d36a4108bb55c4dc
Ran Levy <RanLevy62@gmail.com>
2019-12-24 22:03:52 +0200

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: title,
-      home: TournamentSelectPage(),
+      home: TournamentSelectPage(),  // stam
     );
   }
 }
